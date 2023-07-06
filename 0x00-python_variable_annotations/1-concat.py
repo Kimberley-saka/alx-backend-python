258c81a1147a77471103cb713f0d2bc6d27b7b8f
Kimberley-saka <kimberleysaka@gmail.com>
2023-07-06 09:20:38 +0000

    solutions to task

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
+"""type annotated function concat"""
 
 
 def concat(str1: str, str2: str) -> str:
-    "string concatination"
+    """string concatination"""
     return str1 + str2
---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Ttype-annotated function floor which takes a float
+n as argument and returns the floor of the float
+"""
+
+
+def floor(n: float) -> int:
+    """
+    returns floor of a number
+    """
+    return int(n)
