962fa8755df2f7cbada049405fd6bf8f3a38a8e2
Kimberley saka <kimberleysaka@gmail.com>
2023-07-17 10:28:23 +0200

    solution to task

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Annotated function element_length
+"""
+
+import typing
+
+
+def element_length(lst: typing.Iterable[typing.Sequence]) -> \
+        typing.List[typing.Tuple[typing.Sequence, int]]:
+    """Returns a list of tuples"""
+    return [(i, len(i)) for i in lst]
