084947dd5d6c000c4db5b5833fc9f579a95bd620
Kimberley-saka <kimberleysaka@gmail.com>
2023-07-27 12:30:19 +0000

    fixed for pycodestyle

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
 """
 Imported wait_random from the previous python file that you’ve written
 and write an async routine called wait_n that takes in 2 int arguments
@@ -7,9 +10,6 @@ with the specified max_delay. wait_n should return the list of all
 the delays (float values). The list of the delays should be in ascending
 order without using sort() because of concurrency.
 """
-wait_random = __import__('0-basic_async_syntax').wait_random
-from typing import List
-import asyncio
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
